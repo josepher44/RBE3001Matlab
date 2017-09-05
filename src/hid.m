416eaac07e1973570cf007a30bdf52303f1d2fdc
Matthew Collins <mscollins2@wpi.edu>
2017-09-05 00:25:44 -0400

    added a loop

--- src/hid.m
+++ src/hid.m
@@ -14,30 +14,43 @@ pp = PacketProcessor(7);
 
 %Create an array of 32 bit floating point zeros to load an pass to the
 %packet processor
-values = zeros(15, 1, 'single');
-sinWaveInc = 10.0;
-range = 400.0;
-%Iterate through a sine wave for joint values
- for k=1:sinWaveInc
-     incremtal = (single(k) / sinWaveInc);
-     for j=0:4
-         %Send a new setpoint based on a since wave
-         values((j * 3) + 1) = (sin(incremtal * pi *2.0 )*range)+(range);
-         %Send junk data for velocity and force targets
-         values((j * 3) + 2) = 0;
-         values((j * 3) + 3) = 3;
+x = 0;
+while 1
+    x= x+ 1;
+    values = zeros(15, 1, 'single');
+    sinWaveInc = 10.0;
+    range = 400.0;
+    %Iterate through a sine wave for joint values
+     for k=1:sinWaveInc
+         incremtal = (single(k) / sinWaveInc);
+         for j=0:4
+             %Send a new setpoint based on a since wave
+             values((j * 3) + 1) = (sin(incremtal * pi *2.0 )*range)+(range);
+             %Send junk data for velocity and force targets
+             values((j * 3) + 2) = 0;
+             values((j * 3) + 3) = 3;
+         end
+         tic
+         %Process command and print the returning values
+         returnValues = pp.command(38, values);
+         toc
+         disp('sent');
+         disp(values);
+         disp('got');
+         disp(returnValues);
+         %Concatenate "loop incrementer" to encoder values
+         %toWrite = [returnValues; x];
+         pause(0.1) %timeit(returnValues)
+         dlmwrite('Lab1CSV.csv', transpose(returnValues), '-append');
      end
-     tic
-     %Process command and print the returning values
-     returnValues = pp.command(38, values);
-     toc
-     disp('sent');
-     disp(values);
-     disp('got');
-     disp(returnValues);
-     pause(0.1) %timeit(returnValues)
-     dlmwrite('Lab1CSV.csv', transpose(returnValues), '-append');
- end
+     pause(1)
+     %wait
+     Array=csvread('Lab1CSV.csv');
+     col1 = Array(:, 1);
+     col2 = Array(:, 2);
+     plot(col1, col2)
+end
+
 pp.shutdown()
 clear java;
 %Load the xml file
