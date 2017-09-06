a70a79e50d79b260fd5d546a61f803fb77c59803
Matthew Collins <mscollins2@wpi.edu>
2017-09-05 20:14:40 -0400

    Testing Motor Control

--- src/hid.m
+++ src/hid.m
@@ -22,13 +22,22 @@ while 1
     range = 400.0;
     %Iterate through a sine wave for joint values
      for k=1:sinWaveInc
-         incremtal = (single(k) / sinWaveInc);
-         for j=0:4
-             %Send a new setpoint based on a since wave
-             values((j * 3) + 1) = (sin(incremtal * pi *2.0 )*range)+(range);
+         for j=0:10
+             %Joint 1
+             values(1) = 50; 
              %Send junk data for velocity and force targets
-             values((j * 3) + 2) = 0;
-             values((j * 3) + 3) = 3;
+             values(2) = 0;
+             values(3) = 3;
+             %Joint 2
+             values(4) = 40; 
+             %Send junk data for velocity and force targets
+             values(5) = 0;
+             values(6) = 3;
+             %Joint 3
+             values(7) = 30; 
+             %Send junk data for velocity and force targets
+             values(8) = 0;
+             values(9) = 3;
          end
          tic
          %Process command and print the returning values
@@ -39,16 +48,29 @@ while 1
          disp('got');
          disp(returnValues);
          %Concatenate "loop incrementer" to encoder values
-         %toWrite = [returnValues; x];
-         pause(0.1) %timeit(returnValues)
-         dlmwrite('Lab1CSV.csv', transpose(returnValues), '-append');
+         toWrite = cat(1, x,returnValues);
+         %pause(0.1) %timeit(returnValues)
+         dlmwrite('Lab1CSV.csv', transpose(toWrite), '-append');
      end
-     pause(1)
+     %pause(.01)
      %wait
+     scalingFactor = 11.44;
+     encoder = returnValues(1);
+     deg = encoder/scalingFactor;
      Array=csvread('Lab1CSV.csv');
      col1 = Array(:, 1);
-     col2 = Array(:, 2);
+     col2 = (Array(:, 2))/scalingFactor;
+     hold on;
+     axis([-100 100 -100 100]);
+     x1 = cosd(deg)*100;
+     y1 = sind(deg)*100;
+     vals = [0 x1; 0 y1];
      plot(col1, col2)
+     plotv(vals, '-o')
+     drawnow;
+     hold off;
+     pause(0.1);
+     clf;
 end
 
 pp.shutdown()
