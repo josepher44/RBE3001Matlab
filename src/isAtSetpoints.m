c1f1422cd374716724b9d04782f6697367326952
Joe Gallagher <joedgallagher@yahoo.com>
2017-09-09 22:41:53 -0400

    added isAtSetpoint function

---
+++ src/isAtSetpoints.m
@@ -0,0 +1,19 @@
+function isatsetpoints = isAtSetpoints(setpoints, encoders)
+   accuracy = 0.05;
+   
+   range1 = 2310;
+   range2 = 1485;
+   range3 = 2757;
+   cutoff = [accuracy*range1 accuracy*range2 accuracy*range3];
+
+   isatsetpoints = 0;
+   for i=1:3
+       if abs(setpoints(i)-encoders(i))<cutoff(i)
+           isatsetpoints= isatsetpoints+ 1;
+           disp("At setpoint!");
+       else
+           isatsetpoints= 0;
+       end
+       % isatsetpoints = mod(isatsetpoints,3);
+   end
+end
