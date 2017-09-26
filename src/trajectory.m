83182d65ddf42e06cc2ff78df0bb0d76381ca1b7
Joe Gallagher <joedgallagher@yahoo.com>
2017-09-26 15:37:02 -0400

    More complete polynomial but generates odd velocity results

--- src/trajectory.m
+++ src/trajectory.m
@@ -1,8 +1,8 @@
-function [time,pos,vel,accel] = trajectory(tStart,tEnd,vStart,vEnd,pStart,pEnd,timeStep)
+function out = trajectory(tStart,tEnd,vStart,vEnd,pStart,pEnd,timeStep)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 
-syms t;
+syms t time;
 
 tDiff = tEnd-tStart;
 n = tDiff/timeStep
@@ -15,19 +15,27 @@ A = [1 tStart tStart^2 tStart^3;
      0 1      2*tEnd   3*tStart^2];
  
 %Create column vector of position and velocity
-q = [pStart;vStart;pEnd;vEnd];
+q = [pStart;vStart;pEnd;vEnd]
 
 %Create column vector of coefficients
-coeffs = inverse(A)*q;
+coeffs = inv(A)*q
 
-%generate polynomial
-poly = coeffs(1)*t^3 + coeffs(2)*t^2 + coeffs(3)*t + coeffs(4);
+%generate polynomials
+position = symfun(coeffs(4)*t^3 + coeffs(3)*t^2 + coeffs(2)*t + coeffs(1), t);
+velocity = symfun(3*coeffs(4)*t^2 + 2*coeffs(3)*t + coeffs(2), t);
+acceleration = symfun(6*coeffs(4)*t + 2*coeffs(3), t);
 
 %Now, iterate across the polynomial by plugging in values of t incremented
 %by timeStep
 
+out = [];
 for x = 0:n
-   time = tStart + n*timeStep;
+   time = tStart + x*timeStep
+   pnew = position(time);
+   vnew = velocity(time);
+   anew = acceleration(time);
+   out = [out;time pnew vnew anew];
+   out = double(out)
    %generate some position function
    %take first and second derivatives
    %find setpoints along that trajectory in pos vel and accel
