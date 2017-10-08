6af14fa6d6c4acfa466956bd48175221f28c38c7
Rohit Unnam <Rohit Unnam>
2017-10-07 21:26:20 -0400

    working gripper

--- src/hid.m
+++ src/hid.m
@@ -47,10 +47,10 @@ currentElbow = 30;
 setpointIncrementer=0;
 
 %setpoints = [interpolate(set(1,:),set(2,:),20); interpolate(set(2,:),set(3,:),20); interpolate(set(3,:),set(1,:),20);];
-setpoints = [trajectory3d(45,60,30,45,-60,30); trajectory3d(45,-60,30,45,0,60); trajectory3d(45,0,60,45,60,30)]; 
-%  set2 = [45,60,30;
-%          ];
-%  setpoints = set2;
+%setpoints = [trajectory3d(45,60,30,45,-60,30); trajectory3d(45,-60,30,45,0,60); trajectory3d(45,0,60,45,60,30)]; 
+  set2 = [45,60,30;
+          ];
+  setpoints = set2;
 % setpoints = [interpolate(set2(1,:),set2(2,:),20); interpolate(set2(2,:),set2(3,:),20); interpolate(set2(3,:),set2(1,:),20);]
 % setpoints = [trajectory3d(30,-30,100,-30,-30,100); trajectory3d(-30,-30,100,-30,30,100); 
 %             trajectory3d(-30,30,100,30,30,100);trajectory3d(30,30,100,30,-30,100);
@@ -67,9 +67,29 @@ pidConstants=single(pidConstants);
 pidJunk = pp.command(39, pidConstants);
 %%%%%%%
 
+x=0;
+
 while 1
     
     values = zeros(15, 1, 'single');
+    
+    gripper = zeros(15,1,'single');
+    x=x+1;
+    if(x<10)
+        gripper(1)=1;
+    else
+        gripper(1)=0;
+    end
+    if(x>20)
+        x=0;
+    end
+    forces = pp.command(42, gripper);
+    disp(forces(1));
+    disp(" ");
+    disp(forces(2));
+    disp(" ");
+    disp(forces(3));
+    
     
     if success
         setpointIncrementer=setpointIncrementer+1;
@@ -82,7 +102,7 @@ while 1
         setpointIncrementer=0;
     end
     
-    [success, returnValues] = goToXYZ(currentBase,currentShoulder,currentElbow,pp);
+    %[success, returnValues] = goToXYZ(currentBase,currentShoulder,currentElbow,pp);
     
     %encoder values read from returnValues
     baseEncoder = returnValues(1);
@@ -94,9 +114,9 @@ while 1
     %encoder values mapped to degrees
     baseDeg = baseEncoder / baseScalingFactor;
     shoulderDeg = shoulderEncoder/ baseScalingFactor;
-    elbowDeg = (elbowEncoder/baseScalingFactor)-90;,
+    elbowDeg = (elbowEncoder/baseScalingFactor)-90;
     
-    [tip,deg] = plot3d(returnValues,currentSetpoint,plotVar,setpoints,tip,deg);
+    %[tip,deg] = plot3d(returnValues,currentSetpoint,plotVar,setpoints,tip,deg);
     pause(0.1);
 end
 pp.shutdown()
